c49a66b0cce83f96dbdecf9ec35efad5ffd0347a
Zoneam <zoneam@gmail.com>
2023-01-11 19:49:22 -0800

    changing database location

--- photify/settings.py
+++ photify/settings.py
@@ -81,16 +81,20 @@ WSGI_APPLICATION = 'photify.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'photofy',
+#         'USER': 'myappuser',
+#         'PASSWORD' : 'mypass',
+#         'HOST': 'localhost',
+#         'PORT' : 5432
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'photofy',
-        'USER': 'myappuser',
-        'PASSWORD' : 'mypass',
-        'HOST': 'localhost',
-        'PORT' : 5432
-    }
-}
+    'default': dj_database_url.config(default=os.environ['SQL_DATABASE'], conn_max_age=600)}
 
 
 # DATABASES = {
