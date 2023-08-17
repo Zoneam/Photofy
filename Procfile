ed35feb348f7b582f2e19751361733a320e6f0d0
Zoneam <zoneam@gmail.com>
2023-08-16 17:47:20 -0700

    changing project name

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn photify.wsgi
+web: gunicorn photofy.wsgi
--- photofy/wsgi.py
+++ photofy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for photify project.
+WSGI config for photofy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photify.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photofy.settings')
 
 application = get_wsgi_application()
